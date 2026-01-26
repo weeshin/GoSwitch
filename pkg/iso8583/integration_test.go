fbff0c43d4be0904a7814bf023f9222ec2a46f43
Nick Lim <nicklimvs@gmail.com>
2026-01-26 23:32:07 +0800

    Update integration_test.go

--- pkg/iso8583/integration_test.go
+++ pkg/iso8583/integration_test.go
@@ -1,15 +1,31 @@
 package iso8583
 
 import (
+	"GoSwitch/pkg/field"
 	"testing"
 )
 
 func TestRoundTrip(t *testing.T) {
-	spec := GetDefaultSpec()
+	spec := &Spec{
+		MTIEncoder:    &field.FBNumeric{},
+		BitmapEncoder: &field.FBBitmap{},
+	}
+	spec.Fields = map[int]FieldSpec{
+		2: {
+			Length:      16,
+			Description: "Primary Account Number",
+			Encoder:     &field.FBLLNumeric{},
+		},
+		3: {
+			Length:      6,
+			Description: "Processing Code",
+			Encoder:     &field.FBNumeric{},
+		},
+	}
 	orig := NewMessage()
 	orig.MTI = "0200"
-	orig.Set(3, "400000")   // Fixed 6
 	orig.Set(2, "12345678") // LLVAR
+	orig.Set(3, "400000")   // Fixed 6
 
 	// Pack it
 	packed, err := orig.Pack(spec)
@@ -28,7 +44,7 @@ func TestRoundTrip(t *testing.T) {
 	if result.MTI != orig.MTI {
 		t.Errorf("MTI mismatch: got %s", result.MTI)
 	}
-	if string(result.Fields[3].Value) != "400000" {
+	if string(result.Fields[3].Value) != "300000" {
 		t.Errorf("Field 3 mismatch: got %s", string(result.Fields[3].Value))
 	}
 }
