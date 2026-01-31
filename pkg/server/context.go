30790423206cfebe35b2ed8474ae43480a46d4e9
Nick Lim <nicklimvs@gmail.com>
2026-01-31 22:51:56 +0800

    Update context.go

--- pkg/server/context.go
+++ pkg/server/context.go
@@ -2,20 +2,17 @@ package server
 
 import (
 	"GoSwitch/pkg/iso8583"
-	"net"
 )
 
 type Context struct {
 	Request *iso8583.Message
-	conn    net.Conn
 	Channel Channel
 	Spec    *iso8583.Spec
 }
 
-func NewContext(conn net.Conn, request *iso8583.Message, channel Channel, spec *iso8583.Spec) *Context {
+func NewContext(request *iso8583.Message, channel Channel, spec *iso8583.Spec) *Context {
 	return &Context{
 		Request: request,
-		conn:    conn,
 		Channel: channel,
 		Spec:    spec,
 	}
@@ -23,5 +20,5 @@ func NewContext(conn net.Conn, request *iso8583.Message, channel Channel, spec *
 
 // Send packs the message and sends it back using the configured channel
 func (c *Context) Send(msg *iso8583.Message) error {
-	return c.Channel.Send(c.conn, msg)
+	return c.Channel.Send(msg)
 }
